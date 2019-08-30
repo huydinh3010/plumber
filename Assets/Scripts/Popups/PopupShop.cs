bbed3250da398cbe5e35e9778505be4dba784903
DinhNguyen <dinhnh@imosys.vn>
2019-08-30 14:33:46 +0700

    Enable IAP Button during the game

--- Assets/Scripts/Popups/PopupShop.cs
+++ Assets/Scripts/Popups/PopupShop.cs
@@ -3,14 +3,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-
+using UnityEngine.Purchasing;
 public class PopupShop : MonoBehaviour, IPopup
 {
     [SerializeField] RectTransform content;
     [SerializeField] Button btn_Buy_NoAds;
     [SerializeField] Button[] btn_Buy_Coins;
     [SerializeField] Text[] text_Coins;
-    private ImoSysSDK.Purchasing.IMOIAPButton[] iapButtons;
     private Action btn_Close_Callback;
     private Action btn_Buy_Callback;
     private bool isShow;
@@ -18,13 +17,10 @@ public class PopupShop : MonoBehaviour, IPopup
     {
         IAPManager.Instance.RegisterNoAdsCallback(() => { btn_Buy_NoAds.interactable = false; });
         btn_Buy_NoAds.GetComponentInChildren<Text>().text = "$" + GameConfig.SHOP_PRICE[0].ToString();
-        iapButtons = new ImoSysSDK.Purchasing.IMOIAPButton[GameConfig.SHOP_PRICE.Length];
-        iapButtons[0] = btn_Buy_NoAds.GetComponent<ImoSysSDK.Purchasing.IMOIAPButton>();
         for (int i = 0; i < btn_Buy_Coins.Length; i++)
         {
             text_Coins[i].text = "+" + GameConfig.SHOP_COIN[i + 1].ToString();
             btn_Buy_Coins[i].GetComponentInChildren<Text>().text = "$" + GameConfig.SHOP_PRICE[i + 1].ToString();
-            iapButtons[i+1] = btn_Buy_Coins[i].GetComponent<ImoSysSDK.Purchasing.IMOIAPButton>();
         }
     }
 
@@ -44,20 +40,12 @@ public class PopupShop : MonoBehaviour, IPopup
     {
         isShow = false;
         content.anchoredPosition = Vector2.zero;
-        for (int i = 0; i < iapButtons.Length; i++)
-        {
-            iapButtons[i].enabled = false;
-        }
         btn_Buy_NoAds.interactable = GameData.Instance.isAdsOn;
     }
 
     public void Close()
     {
         isShow = false;
-        for (int i = 0; i < iapButtons.Length; i++)
-        {
-            iapButtons[i].enabled = false;
-        }
         EventDispatcher.Instance.PostEvent(EventID.OnPopupClosed, this);
         GetComponent<Animator>().Play("Close");
     }
@@ -70,11 +58,6 @@ public class PopupShop : MonoBehaviour, IPopup
     public void OnDisplayed()
     {
         isShow = true;
-        for (int i = 1; i < iapButtons.Length; i++)
-        {
-            iapButtons[i].enabled = true;
-        }
-        iapButtons[0].enabled = GameData.Instance.isAdsOn;
     }
 
     public void Show(Dictionary<PopupButtonEvent, Action> list_actions, Dictionary<PopupSettingType, object> list_settings)
@@ -85,11 +68,21 @@ public class PopupShop : MonoBehaviour, IPopup
         GetComponent<Animator>().Play("Show");
     }
 
-    public void BtnBuyOnClick()
+    public void BtnBuyNoAdsOnClick()
     {
         if (isShow)
         {
             btn_Buy_Callback?.Invoke();
+            CodelessIAPStoreListener.Instance.InitiatePurchase("com.waterline.removeads");
+        }
+    }
+
+    public void BtnBuyCoinOnClick(int coins)
+    {
+        if(isShow)
+        {
+            btn_Buy_Callback?.Invoke();
+            CodelessIAPStoreListener.Instance.InitiatePurchase("com.waterline.coins" + coins);
         }
     }
 
